c682277278ad6ffaa98662a0e88b6d759b7fbd5b
mariodz95 <mariodz95@gmail.com>
2020-04-17 17:58:41 +0200

    Add swagger auth

--- Backend/ProjectManagement/ProjectManagement/Startup.cs
+++ Backend/ProjectManagement/ProjectManagement/Startup.cs
@@ -17,6 +17,7 @@ using System.Text;
 using Microsoft.OpenApi.Models;
 using System.Threading.Tasks;
 using Common.Helpers;
+using System.Collections.Generic;
 
 namespace ProjectManagement
 {
@@ -105,6 +106,34 @@ namespace ProjectManagement
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Project Management", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description =
+        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+        },
+        new List<string>()
+    }
+});
             });
 
         }
